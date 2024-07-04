afce36fc12127ead05b0470927edb9d51375f91b
Amaranth01 <vanessa.nectoux@gmail.com>
2024-07-04 15:54:03 +0200

    fisnish parallax

---
+++ asset/progressBar.js
@@ -0,0 +1,5 @@
+window.onscroll = function() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    document.getElementById("progressBar").value = (winScroll / height) * 100;
+};
